3f0b630393cadf84359632e0ddc70655f47e23e1
老猫不发威你当我病危 <oldcat@huix.cc>
2021-01-01 17:09:10 +0800

    add element-plus and fix some lint error

---
+++ src/plugins/element.ts
@@ -0,0 +1,8 @@
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import locale from 'element-plus/lib/locale/lang/zh-cn'
+import { App } from 'vue'
+
+export default (app: App): void => {
+  app.use(ElementPlus, { locale })
+}
